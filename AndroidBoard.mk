a42a66d8bb0de4712e2fe2f6dead73a3dddd1aba
Brian Crook <brian.w.crook@gmail.com>
2010-10-26 23:09:54 -0400

    Dream/Magic device profile

---
+++ AndroidBoard.mk
@@ -0,0 +1,106 @@
+# Copyright (C) 2007 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+LOCAL_PATH := $(call my-dir)
+
+# from device/htc/dream_magic
+file := $(TARGET_OUT_KEYLAYOUT)/sapphire-keypad.kl
+ALL_PREBUILT += $(file)
+$(file) : $(LOCAL_PATH)/sapphire-keypad.kl | $(ACP)
+	$(transform-prebuilt-to-target)
+
+file := $(TARGET_ROOT_OUT)/init.magic.rc
+ALL_PREBUILT += $(file)
+$(file) : $(LOCAL_PATH)/init.magic.rc | $(ACP)
+	$(transform-prebuilt-to-target)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := sapphire-keypad.kcm
+include $(BUILD_KEY_CHAR_MAP)
+
+# from device/htc/dream_magic
+file := $(TARGET_OUT_KEYLAYOUT)/trout-keypad.kl
+ALL_PREBUILT += $(file)
+$(file) : $(LOCAL_PATH)/trout-keypad.kl | $(ACP)
+	$(transform-prebuilt-to-target)
+
+file := $(TARGET_OUT_KEYLAYOUT)/trout-keypad-v2.kl
+ALL_PREBUILT += $(file)
+$(file) : $(LOCAL_PATH)/trout-keypad-v2.kl | $(ACP)
+	$(transform-prebuilt-to-target)
+
+file := $(TARGET_OUT_KEYLAYOUT)/trout-keypad-v3.kl
+ALL_PREBUILT += $(file)
+$(file) : $(LOCAL_PATH)/trout-keypad-v3.kl | $(ACP)
+	$(transform-prebuilt-to-target)
+
+file := $(TARGET_OUT_KEYLAYOUT)/trout-keypad-qwertz.kl
+ALL_PREBUILT += $(file)
+$(file) : $(LOCAL_PATH)/trout-keypad-qwertz.kl | $(ACP)
+	$(transform-prebuilt-to-target)
+
+file := $(TARGET_ROOT_OUT)/init.trout.rc
+ALL_PREBUILT += $(file)
+$(file) : $(LOCAL_PATH)/init.trout.rc | $(ACP)
+	$(transform-prebuilt-to-target)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := trout-keypad.kcm
+include $(BUILD_KEY_CHAR_MAP)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := trout-keypad-v2.kcm
+include $(BUILD_KEY_CHAR_MAP)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := trout-keypad-v3.kcm
+include $(BUILD_KEY_CHAR_MAP)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := trout-keypad-qwertz.kcm
+include $(BUILD_KEY_CHAR_MAP)
+
+# from AndroidBoardCommon.mk
+file := $(TARGET_OUT_KEYLAYOUT)/h2w_headset.kl
+ALL_PREBUILT += $(file)
+$(file) : $(LOCAL_PATH)/h2w_headset.kl | $(ACP)
+	$(transform-prebuilt-to-target)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE := vold.fstab
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := wlan.ko
+LOCAL_MODULE_TAGS := user
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT)/lib/modules
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+# the system properties for each device, loaded by init
+file := $(TARGET_OUT)/build.magic.prop
+ALL_PREBUILT += $(file)
+$(file) : $(LOCAL_PATH)/build.magic.prop | $(ACP)
+	$(transform-prebuilt-to-target)
+
+file := $(TARGET_OUT)/build.trout.prop
+ALL_PREBUILT += $(file)
+$(file) : $(LOCAL_PATH)/build.trout.prop | $(ACP)
+	$(transform-prebuilt-to-target)
+
+-include vendor/htc/dream_magic/AndroidBoardVendor.mk
+
